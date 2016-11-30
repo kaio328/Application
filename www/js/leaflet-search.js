a8ac47c24ab43e99bb32b2798ed0cf4b225f60ba
kaio328 <kai.ru@freenet.de>
2016-11-30 20:35:04 +0100

    introducing marker popup via leaflet-search

--- www/js/leaflet-search.js
+++ www/js/leaflet-search.js
@@ -748,7 +748,7 @@ L.Control.Search = L.Control.extend({
 			this._markerLoc.setLatLng(latlng);  
 			this._markerLoc.setTitle(title);
 			this._markerLoc.show();
-			this._markerLoc.bindPopup(title);
+			this._markerLoc.bindPopup();
 			if(this.options.animateLocation)
 				this._markerLoc.animate();
 	
