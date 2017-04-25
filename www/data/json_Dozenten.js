e28ec10387ef40995566ee77162535ba4334d752
kaio328 <kai.ru@freenet.de>
2017-04-25 14:43:27 +0200

    update

---
+++ www/data/json_Dozenten.js
@@ -0,0 +1,10 @@
+var json_Dozenten={
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                                                                                
+"features": [
+
+{ "type": "Feature", "properties": { "id": null, "Bez": "Dr. Dennis Edler", "Syn": "Edler", "Vorname": "Dennis", "Nachname": "Edler", "Raumnummer": "NA 5/169", "Sprechzeiten": "Freitag 10-12 (u.n.V.)", "E-Mail":"Dennis.Edler@rub.de", "Telefonnummer":"+49 234-32-23362" }, "geometry": { "type": "Point", "coordinates": [ 7.26367, 51.4437 ] } }
+
+]
+}
---
+++ www/data/json_Gebäude.js
@@ -0,0 +1,10 @@
+var json_Gebäude={
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                                                                                
+"features": [
+
+{ "type": "Feature", "properties": { "id": null, "Bez": "NA", "Syn": null, "Behindertengerechte Toiletten":"Raumnummer" }, "geometry": { "type": "Point", "coordinates": [ 7.26368, 51.44388 ] } }
+
+]
+}
