d7041c01d22cdfc49d1cfcf851aa38c3ca368db7
kaio328 <kai.ru@freenet.de>
2017-05-14 01:33:23 +0200

    asd

--- www/data/json_Einrichtungen.js
+++ www/data/json_Einrichtungen.js
@@ -1,14 +1,133 @@
-var json_Einrichtungen={
-"type": "FeatureCollection",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-                                                                                
-"features": [
+{
+	"type": "FeatureCollection",
+	"crs": {
+		"type": "name",
+		"properties": {
+			"name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+		}
+	},
 
-{ "type": "Feature", "properties": { "id": null, "Bez": "Mensa", "Syn": "Futter" }, "geometry": { "type": "Point", "coordinates": [ 7.262467998878375, 51.443086504221256 ] } },
-{ "type": "Feature", "properties": { "id": null, "Bez": "Audimax", "Syn": "Audimax" }, "geometry": { "type": "Point", "coordinates": [ 7.261779486151085, 51.443848793618244 ] } },
-{ "type": "Feature", "properties": { "id": null, "Bez": "CampusCenter", "Syn": "CC" }, "geometry": { "type": "Point", "coordinates": [ 7.259840779261091, 51.443973017351269 ] } },
-{ "type": "Feature", "properties": { "id": null, "Bez": "Akafoe", "Syn": "Bafög-Amt" }, "geometry": { "type": "Point", "coordinates": [ 7.255822945122241, 51.445045844620687 ] } },
-{ "type": "Feature", "properties": { "id": null, "Bez": "Universitätsbibliothek", "Syn": "Bib (Hauptbibliothek)" }, "geometry": { "type": "Point", "coordinates": [ 7.260769365505132, 51.445023259043168 ] } },
-{ "type": "Feature", "properties": { "id": null, "Bez": "Kindertagesstätte", "Syn": "KITA" }, "geometry": { "type": "Point", "coordinates": [ 7.261836107263529, 51.441787759646211 ] } }
-]
-}
+	"features": [{
+			"type": "Feature",
+			"properties": {
+				"id": 0,
+				"Bez": "Mensa",
+				"Syn": null,
+				"Öffnungszeiten": "Mo - Do 11.00 - 14.30 Uhr, Fr 11.00 - 14.00 Uhr"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [7.262467998878375, 51.443086504221256]
+			}
+		}, {
+			"type": "Feature",
+			"properties": {
+				"id": 1,
+				"Bez": "CampusCenter",
+				"Syn": "CC, Campuscenter"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [7.259840779261091, 51.443973017351269]
+			}
+		}, {
+			"type": "Feature",
+			"properties": {
+				"id": 2,
+				"Bez": "Akafoe",
+				"Syn": "Bafög-Amt"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [7.255822945122241, 51.445045844620687]
+			}
+		}, {
+			"type": "Feature",
+			"properties": {
+				"id": 3,
+				"Bez": "Universitätsbibliothek",
+				"Syn": "Bib (Hauptbibliothek)"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [7.260769365505132, 51.445023259043168]
+			}
+		}, {
+			"type": "Feature",
+			"properties": {
+				"id": 4,
+				"Bez": "Kindertagesstätte",
+				"Syn": "KITA"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [7.261836107263529, 51.441787759646211]
+			}, {
+				"type": "Feature",
+				"properties": {
+					"id": 5,
+					"Bez": "Akafoe",
+					"Syn": "Bafög-Amt"
+				},
+				"geometry": {
+					"type": "Point",
+					"coordinates": [7.255822945122241, 51.445045844620687]
+				}
+			}, {
+				"type": "Feature",
+				"properties": {
+					"id": 6,
+					"Bez": "Universitätsbibliothek",
+					"Syn": "Bib (Hauptbibliothek)"
+				},
+				"geometry": {
+					"type": "Point",
+					"coordinates": [7.260769365505132, 51.445023259043168]
+				}
+			}, {
+				"type": "Feature",
+				"properties": {
+					"id": 7,
+					"Bez": "Kindertagesstätte",
+					"Syn": "KITA"
+				},
+				"geometry": {
+					"type": "Point",
+					"coordinates": [7.261836107263529, 51.441787759646211]
+				}
+			}, {
+				"type": "Feature",
+				"properties": {
+					"id": 8,
+					"Bez": "Akafoe",
+					"Syn": "Bafög-Amt"
+				},
+				"geometry": {
+					"type": "Point",
+					"coordinates": [7.255822945122241, 51.445045844620687]
+				}
+			}, {
+				"type": "Feature",
+				"properties": {
+					"id": 9,
+					"Bez": "Universitätsbibliothek",
+					"Syn": "Bib (Hauptbibliothek)"
+				},
+				"geometry": {
+					"type": "Point",
+					"coordinates": [7.260769365505132, 51.445023259043168]
+				}
+			}, {
+				"type": "Feature",
+				"properties": {
+					"id": 10,
+					"Bez": "Kindertagesstätte",
+					"Syn": "KITA"
+				},
+				"geometry": {
+					"type": "Point",
+					"coordinates": [7.261836107263529, 51.441787759646211]
+					}
+			}
+			]
+	}
