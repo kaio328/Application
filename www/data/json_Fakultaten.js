0ad2193f9b15fdd6d9dfd75ee803be1073428ade
kaio328 <kai.ru@freenet.de>
2017-05-15 21:41:18 +0200

    data

---
+++ www/data/json_Fakultaten.js
@@ -0,0 +1,79 @@
+{
+	"type": "FeatureCollection",
+	"crs": {
+		"type": "name",
+		"properties": {
+			"name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+		}
+	},
+
+	"features": [
+		{
+			"type": "Feature",
+			"properties": {
+				"id": 0,
+				"Bez": "TEST",
+				"Ausstattung": "Futter"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [7.262467998878375, 51.443086504221256]
+			}
+		}, {
+			"type": "Feature",
+			"properties": {
+				"id": 1,
+				"Bez": "TEST",
+				"Syn": "Audimax"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [7.261779486151085, 51.443848793618244]
+			}
+		}, {
+			"type": "Feature",
+			"properties": {
+				"id": 2,
+				"Bez": "TEST",
+				"Syn": "CC"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [7.259840779261091, 51.443973017351269]
+			}
+		}, {
+			"type": "Feature",
+			"properties": {
+				"id": 3,
+				"Bez": "TEST",
+				"Syn": "Bafög-Amt"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [7.255822945122241, 51.445045844620687]
+			}
+		}, {
+			"type": "Feature",
+			"properties": {
+				"id": 4,
+				"Bez": "TEST",
+				"Syn": "Bib (Hauptbibliothek)"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [7.260769365505132, 51.445023259043168]
+			}
+		}, {
+			"type": "Feature",
+			"properties": {
+				"id": 5,
+				"Bez": "TEST",
+				"Syn": "KITA"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [7.261836107263529, 51.441787759646211]
+			}
+		}
+	]
+}
